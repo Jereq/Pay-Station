39f0c4694a8f6a0ea40efae3d97b45aeb60db369
Christoffer Jonstrand <christoffer_jonstrand@hotmail.com>
2011-11-16 11:16:15 +0100

    added 2 comments

--- src/payStation/paystationInterface/PayStation.java
+++ src/payStation/paystationInterface/PayStation.java
@@ -1,9 +1,22 @@
 package payStation.paystationInterface;
+import payStation.paystationInterface.*;
 
 public interface PayStation {
 void addPayment(int coinValue);
+/**
+ * returns the amount paid in cents
+ * @return the amount paid in cents
+ */
 int getPayment();
+/**
+ * returns the parking time in minutes 
+ * @return the parking time in minutes
+ */
 int getMinutes();
-
+/**
+ * 
+ * @return
+ */
+Receipt buy();
 void cancel();
 }
