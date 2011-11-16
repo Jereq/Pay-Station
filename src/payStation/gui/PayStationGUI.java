f5936c5384c66995a62472c621c41c8408499292
Sebastian Larsson <sjereq@gmail.com>
2011-11-16 12:07:38 +0100

    Named and edited the GUI components

--- src/payStation/gui/PayStationGUI.java
+++ src/payStation/gui/PayStationGUI.java
@@ -21,7 +21,7 @@ import javax.swing.JButton;
 public class PayStationGUI {
 
 	private JFrame frmPayStation;
-	private JTextField textField;
+	private JTextField coinInputTextField;
 
 	/**
 	 * Launch the application.
@@ -66,8 +66,8 @@ public class PayStationGUI {
 		JLabel lblYouHavePaid = new JLabel("You have paid:");
 		frmPayStation.getContentPane().add(lblYouHavePaid, "cell 1 1");
 		
-		JLabel label = new JLabel("0");
-		frmPayStation.getContentPane().add(label, "cell 3 1");
+		JLabel centLabel = new JLabel("0");
+		frmPayStation.getContentPane().add(centLabel, "cell 3 1");
 		
 		JLabel lblCents = new JLabel("cent");
 		frmPayStation.getContentPane().add(lblCents, "cell 4 1");
@@ -75,15 +75,15 @@ public class PayStationGUI {
 		JLabel lblYouHavePaid_1 = new JLabel("Your parking time:");
 		frmPayStation.getContentPane().add(lblYouHavePaid_1, "cell 1 2");
 		
-		JLabel label_1 = new JLabel("0");
-		frmPayStation.getContentPane().add(label_1, "cell 3 2");
+		JLabel minutesLabel = new JLabel("0");
+		frmPayStation.getContentPane().add(minutesLabel, "cell 3 2");
 		
 		JLabel lblMinutes = new JLabel("minutes");
 		frmPayStation.getContentPane().add(lblMinutes, "cell 4 2");
 		
-		textField = new JTextField();
-		frmPayStation.getContentPane().add(textField, "cell 6 2,alignx left");
-		textField.setColumns(10);
+		coinInputTextField = new JTextField();
+		frmPayStation.getContentPane().add(coinInputTextField, "cell 6 2,growx");
+		coinInputTextField.setColumns(10);
 		
 		JLabel lblCents_1 = new JLabel("cent");
 		frmPayStation.getContentPane().add(lblCents_1, "cell 7 2,alignx left");
@@ -91,10 +91,10 @@ public class PayStationGUI {
 		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
 		frmPayStation.getContentPane().add(horizontalStrut_1, "cell 8 2");
 		
-		JButton btnBuy = new JButton("Buy");
-		frmPayStation.getContentPane().add(btnBuy, "cell 1 4");
+		JButton buyButton = new JButton("Buy");
+		frmPayStation.getContentPane().add(buyButton, "cell 1 4");
 		
-		JButton btnCancel = new JButton("Cancel");
-		frmPayStation.getContentPane().add(btnCancel, "cell 2 4 3 1");
+		JButton cancelButton = new JButton("Cancel");
+		frmPayStation.getContentPane().add(cancelButton, "cell 2 4 3 1");
 	}
 }
