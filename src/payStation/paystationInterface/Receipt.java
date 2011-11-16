b0365f2b49b3a4165c808c337d29ceed3fccd97c
Sebastian Larsson <sjereq@gmail.com>
2011-11-16 11:05:58 +0100

    Added comment to Receipt.getMinutes()

--- src/payStation/paystationInterface/Receipt.java
+++ src/payStation/paystationInterface/Receipt.java
@@ -1,5 +1,11 @@
 package payStation.paystationInterface;
 
 public interface Receipt {
-
+	/**
+	 * Returns the number of minutes in the receipt
+	 * 
+	 * @return the number of minutes in the receipt
+	 */
+	public int getMinutes();
+	public int getPayment();
 }
