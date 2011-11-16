272dec7bca6f00737768689212c76c0c4bdc7022
Sebastian Larsson <sjereq@gmail.com>
2011-11-16 11:26:52 +0100

    Added PayStationImpl

---
+++ src/payStation/impl/PayStationImpl.java
@@ -0,0 +1,38 @@
+package payStation.impl;
+
+import payStation.paystationInterface.PayStation;
+import payStation.paystationInterface.Receipt;
+
+public class PayStationImpl implements PayStation {
+
+	@Override
+	public void addPayment(int coinValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getPayment() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinutes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Receipt buy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
