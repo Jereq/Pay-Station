d4e77d4d2723910b1c0dc23b44cb3dd7876236b9
Sebastian Larsson <sjereq@gmail.com>
2011-11-16 15:25:28 +0100

    Wrote javadoc for IllegalCoinException

--- src/payStation/payStationInterface/exception/IllegalCoinException.java
+++ src/payStation/payStationInterface/exception/IllegalCoinException.java
@@ -1,12 +1,27 @@
 package payStation.payStationInterface.exception;
 
 public class IllegalCoinException extends Exception {
+	/**
+	 * Silence warning about serialVersionUID not present
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private int coinValue;
 	
+	/**
+	 * Constructor passing the illegal coin value that caused this exception
+	 * 
+	 * @param coinValue the illegal coin value
+	 */
 	public IllegalCoinException(int coinValue) {
 		this.coinValue = coinValue;
 	}
 	
+	/**
+	 * Gets the illegal coin value that caused this exception
+	 * 
+	 * @return the illegal coin value
+	 */
 	public int getCoinValue() {
 		return coinValue;
 	}
