c302696da7d77973fc5d459bae2926470bdb64f1
Christoffer Jonstrand <christoffer_jonstrand@hotmail.com>
2011-11-23 11:16:09 +0100

    made currency interface code and commented

--- src/payStation/payStationInterface/Currency.java
+++ src/payStation/payStationInterface/Currency.java
@@ -1,5 +1,30 @@
 package payStation.payStationInterface;
 
+/**
+ * Base for different currencies.
+ * To add another currency implement <code>Currency</code>.
+ *
+ */
 public interface Currency {
-
+	/**
+	 * checks so that the coin has a valid value
+	 * 
+	 * @param coin
+	 * @return valid or unvalid as true or false
+	 */
+	boolean checkValidCoin(Coin coin);
+	/**
+	 * Gets the standard coin to second rate for the currency
+	 * 
+	 * @param coin2
+	 * @return the coin to second rate
+	 */
+	int getStandardRate(Coin coin2);
+	/**
+	 * gives the value as a string plus the currency signs
+	 * 
+	 * @param value
+	 * @return the value string plus the currency signs
+	 */
+	String formatCoinValue(int value);
 }
