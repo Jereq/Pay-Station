15b410ce02586e7d930eea49a99b14f836f4bb62
Sebastian Larsson <sjereq@gmail.com>
2011-11-23 10:53:11 +0100

    Filled out Coin

--- src/payStation/payStationInterface/Coin.java
+++ src/payStation/payStationInterface/Coin.java
@@ -1,5 +1,21 @@
 package payStation.payStationInterface;
 
+/**
+ * The interface <code>Coin</code> provides a base for a coin with a certain
+ * currency.
+ */
 public interface Coin {
+	/**
+	 * Returns the value of the coin in the coins currency.
+	 * 
+	 * @return the value of the coin
+	 */
+	int getValue();
 
+	/**
+	 * Returns the currency of the coin
+	 * 
+	 * @return the currency of the coin
+	 */
+	Currency getCurrency();
 }
