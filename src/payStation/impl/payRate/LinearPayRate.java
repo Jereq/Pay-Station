a9df4affbbe1889a41b018d785d94430474543a9
Sebastian Larsson <sjereq@gmail.com>
2011-11-23 11:03:19 +0100

    Implemented LinearPayRate

--- src/payStation/impl/payRate/LinearPayRate.java
+++ src/payStation/impl/payRate/LinearPayRate.java
@@ -2,6 +2,15 @@ package payStation.impl.payRate;
 
 import payStation.payStationInterface.PayRate;
 
+/**
+ * Implements the standard pay rate, every 60 s gives one minute of parking
+ * time.
+ */
 public class LinearPayRate implements PayRate {
+
+	@Override
+	public int calculateTime(int totalSeconds) {
+		return totalSeconds / 60;
+	}
 
 }
