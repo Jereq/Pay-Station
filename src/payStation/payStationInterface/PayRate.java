85d138e602a3e584bf8fa4f2281f21df2b8272d9
Sebastian Larsson <sjereq@gmail.com>
2011-11-23 11:00:30 +0100

    Filled out PayRate

--- src/payStation/payStationInterface/PayRate.java
+++ src/payStation/payStationInterface/PayRate.java
@@ -1,5 +1,17 @@
 package payStation.payStationInterface;
 
+/**
+ * The interface <code>PayRate</code> provides a base for a pay rate
+ * implementation allowing for different payment rates in different pay
+ * stations.
+ */
 public interface PayRate {
-
+	/**
+	 * Calculates the parking time in minutes based on the number of second in
+	 * the standard rate.
+	 * 
+	 * @param totalSeconds number of seconds in base rate
+	 * @return number of minutes of parking time using this pay rate
+	 */
+	int calculateTime(int totalSeconds);
 }
