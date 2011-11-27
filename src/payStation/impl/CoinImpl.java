36c86a41ffcc94815e84ab54abece95e2bf741c4
Sebastian Larsson <sjereq@gmail.com>
2011-11-27 16:06:51 +0100

    Implemented CoinImpl

--- src/payStation/impl/CoinImpl.java
+++ src/payStation/impl/CoinImpl.java
@@ -1,7 +1,37 @@
 package payStation.impl;
 
 import payStation.payStationInterface.Coin;
+import payStation.payStationInterface.Currency;
 
+/**
+ * The class <code>CoinImpl</code> implements <code>Coin</code>.
+ */
 public class CoinImpl implements Coin {
+
+	private int value;
+	private Currency currency;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param value
+	 *            the coins value in the coins currency
+	 * @param currency
+	 *            the currency of the coin
+	 */
+	public CoinImpl(int value, Currency currency) {
+		this.value = value;
+		this.currency = currency;
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public Currency getCurrency() {
+		return currency;
+	}
 
 }
