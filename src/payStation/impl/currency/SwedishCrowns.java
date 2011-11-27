199523b2f7354f0d17d5132e84f27c43c1426b61
Roffe <Roffe@Roffe-Dator>
2011-11-27 16:06:31 +0100

    implemented SwedishCrowns currency class

--- src/payStation/impl/currency/SwedishCrowns.java
+++ src/payStation/impl/currency/SwedishCrowns.java
@@ -1,7 +1,29 @@
 package payStation.impl.currency;
 
+import payStation.payStationInterface.Coin;
 import payStation.payStationInterface.Currency;
 
 public class SwedishCrowns implements Currency {
+
+	@Override
+	public boolean checkValidCoin(Coin coin) {
+		if (!(coin.getCurrency() instanceof SwedishCrowns))
+			return false;
+		int val = coin.getValue();
+		if(val==1 || val==5 || val==10)
+			return true;
+		return false;
+	}
+
+	@Override
+	public int getStandardRate(Coin coin) {
+		return coin.getValue()*24; //24=(60*2)/5 in other words 24 seconds per cent.
+	}
+
+	@Override
+	public String formatCoinValue(int value) {
+		String x = value+"¢";
+		return x;
+	}
 
 }
